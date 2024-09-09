d4f12219bd5c69f4520573d7b58f4d73b642fea8
unknown <shakhawatsalam726@gmail.com>
2024-09-09 12:20:15 +0530

    [Module-06 is done]

---
+++ Introduction to Algorithms/Module-5/cycle_detcet_usein_dfs.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 5;
+bool vis[N];
+vector<int> adj[N];
+int parentArray[N];
+bool ans;
+void dfs(int parent)
+{
+    vis[parent] = true;
+    // cout << parent << " ";
+    for (int child : adj[parent])
+    {
+        if (vis[child] && parentArray[child] != child)
+        {
+            ans = true;
+        }
+        if (vis[child] == false)
+        {
+            parentArray[child] = parent;
+            dfs(child);
+        }
+    }
+}
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    memset(vis, false, sizeof(vis));
+    memset(parentArray, -1, sizeof(parentArray));
+    ans = false;
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i])
+        {
+            dfs(i);
+        }
+    }
+
+    if (ans)
+    {
+        cout << "Cycle Found";
+    }
+    else
+    {
+        cout << "Cycle Not Found";
+    }
+    return 0;
+}
